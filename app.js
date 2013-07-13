ce1627a2b3551f514049b3e6339c9d22b6492651
caok <caok1231@gmail.com>
2013-07-13 16:55:04 +0800

    Abstracts websockets with fallbacks

--- app.js
+++ app.js
@@ -6,7 +6,8 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , socket = require('socket.io');
 
 var app = express();
 
@@ -28,6 +29,13 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+server = http.createServer(app);
+io = socket.listen(server);
+
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+io.sockets.on('connection', function(client) {
+  console.log('Client connected...');
 });
